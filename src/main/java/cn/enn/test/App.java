b65a0cd0bc2e20513b747bc54204d59ba4d98249
Zraphae <roc_820@163.com>
2018-01-16 17:44:03 +0800

    add classloader demo

--- src/main/java/cn/enn/test/App.java
+++ src/main/java/cn/enn/test/App.java
@@ -1,9 +1,42 @@
 package cn.enn.test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
 
 	public static void main(String[] args) {
 		
+		final List<String> imgPaths = Arrays.asList("/test/imgPath1", "/test/imgPath2");
+		final int nThreads = 2;
+		ExecutorService pool = Executors.newFixedThreadPool(nThreads);
+
+		for (int i = 0; i < nThreads; i++) {
+			final String imgPath = imgPaths.get(i);
+			pool.submit(new Runnable() {
+				
+				@Override
+				public void run() {
+					
+					for(int i =0; i< 10; i++) {
+						log.info("imgPath: {}", imgPath);
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+			log.info("start thread finished {}, imgPath: {}", Thread.currentThread().getName(), imgPath);
+		}
+		
+		pool.shutdown();
 	}
 
 }
